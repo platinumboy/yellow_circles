ba39d77b90276d6f2910b3a671beeaccf9a9f265
Egor Batkin <batkin.egor@yandex.ru>
2020-11-26 10:10:29 +0300

    circles-maker

--- master.py
+++ master.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor, QPolygon
 from PyQt5.QtCore import Qt, QPoint
 from random import choice, randint
@@ -15,17 +16,21 @@ class MyWidget(QMainWindow):
 
     def makeflag(self):
         self.draw = True
+        self.update()
 
     def paintEvent(self, event):
         if self.draw:
             qp = QPainter()
             qp.begin(self)
             qp.setBrush(QColor('Yellow'))
-            radius = randint(20, 200)
-            self.x = randint(20, 1000)
-            self.y = randint(20, 700)
-            qp.drawEllipse(self.x - radius, self.y - radius, 2 * radius, 2 * radius)
+
+            for i in range(6):
+                radius = randint(20, 200)
+                self.x = randint(20, 1000)
+                self.y = randint(20, 700)
+                qp.drawEllipse(self.x - radius, self.y - radius, 2 * radius, 2 * radius)
             qp.end()
+            self.draw = False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
